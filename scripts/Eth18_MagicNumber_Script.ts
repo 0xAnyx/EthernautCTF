a7fb53afac377417c6cf9cb23e2b1a45f647a991
Anyx <itsmenick30@gmail.com>
2023-01-04 23:50:38 +0530

    Update Eth18_MagicNumber_Script.ts

--- scripts/Eth18_MagicNumber_Script.ts
+++ scripts/Eth18_MagicNumber_Script.ts
@@ -7,10 +7,10 @@ async function main() {
     const magic = await ethers.getContractFactory('MagicNum')
     const meaningOfLife = await magic.attach('0x1394F20035F76Fe3F0c2e60072818a64c739A4Fa')
 
-    // const byteCode = '69602a60005260206000f3600052600a6016f3'
-    // const tx = await web3.eth.sendTransaction({from: '0xabd7e5f5490B16264b2FfC368b24F3a4862573B7',
-    // data: byteCode})
-    // console.log(tx.contractAddress)
+    const byteCode = '69602a60005260206000f3600052600a6016f3'
+    const tx = await web3.eth.sendTransaction({from: '0xabd7e5f5490B16264b2FfC368b24F3a4862573B7',
+    data: byteCode})
+    console.log(tx.contractAddress)
 
     const tx2 = await meaningOfLife.setSolver('0xB6c4014D61B160422b6269a36066cE74dCd942c3')
     await tx2.wait()
